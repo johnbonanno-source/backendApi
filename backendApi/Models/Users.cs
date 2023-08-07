c88f370cd059f6549d9a0b3b96a4f7b3d67d4a95
JohnBonanno-Enviornments <141369474+JohnBonanno-Enviornments@users.noreply.github.com>
2023-08-07 17:51:10 -0600

    switch to windows

---
+++ backendApi/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backendApi.Models
+{
+
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int user_id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string username { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string password { get; set; }
+    }
+
+}
